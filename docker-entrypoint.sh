2c8cc9115299f956aef3adb8487fa0e5555d621b
Arnaud <arnaud@Mac-mini-de-Arnaud.local>
2026-03-25 20:39:03 +0100

    fix: use sed instead of envsubst to prevent nginx $variable destruction

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-# Injecte les variables d'environnement dans la configuration nginx (token Infoclimat)
-envsubst '${INFOCLIMAT_TOKEN}' < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
+# Injecte le token Infoclimat dans la conf nginx au démarrage (sans toucher aux variables nginx $uri, $args, etc.)
+sed "s|__INFOCLIMAT_TOKEN__|${INFOCLIMAT_TOKEN}|g" /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
 exec nginx -g 'daemon off;'
